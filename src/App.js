b11483e046c6fa2e6efa5002f8a11b230971bf8f
Alexmaxwell7 <Alexmaxwell3096@gmail.com>
2021-04-22 11:48:04 +0530

    feat(completed)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Provider } from "react-redux";
+import PostMessages from "./components/PostMessages";
+import { store } from "./actions/store";
+import { Container, AppBar, Typography } from "@material-ui/core";
+import ButterToast,{ POS_RIGHT,POS_TOP } from "butter-toast";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+     
+      <Container maxWidth="lg">
+        <AppBar position="relative" color="primary" borderRadius={16}>
+          <Typography
+            variant="h3"
+            align="center">
+           Mobile Forums
+          </Typography>
+        </AppBar>
+        <br/>
+        <br/>
+       
+        <div style={{backgroundColor:"gray",borderRadius: '20px'}}>
+        <PostMessages />
+        </div>
+        <ButterToast position={{vertical:POS_TOP,horizontal:POS_RIGHT}}/>
+      </Container>
+   
+    </Provider>
+  
   );
 }
 
